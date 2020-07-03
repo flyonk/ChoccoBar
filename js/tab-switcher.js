1e9eaec7e612c52ac086c392094bcdf61bb3f9da
Petr flyonk <petagoblen@gmail.com>
2020-07-03 20:17:26 +0300

    week4 corrections and vidjets

---
+++ js/tab-switcher.js
@@ -0,0 +1,20 @@
+const findBlockByAlias = (alias) => {
+    return $(".reviews__item").filter((ndx, item) => {
+        return $(item).attr("data-linked-with") == alias
+    });
+    
+};
+
+$(".interactive-avatar__link").click(e => {
+    e.preventDefault();
+
+    const $this = $(e.currentTarget);
+    const target = $this.attr("data-open");
+    const itemToShow = findBlockByAlias(target);
+    const curITem = $this.closest(".interactive-avatar");
+
+    itemToShow.addClass('reviews__item-active').siblings().removeClass('reviews__item-active');
+    curITem.addClass('active').siblings().removeClass('active');
+
+});
+    
